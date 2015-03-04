79650171390d1f1ec0c4f010775b5e4d666a7f82
Elisa Cruz <elisacruzph@gmail.com>
2015-03-04 10:56:50 +0800

    Add click listener for level info page buttons
    Play & cancel buttons

---
+++ Ahon/Assets/Script/LevelInfo.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelInfo : MonoBehaviour {
+
+	public void OnClickPlay()
+	{
+		Application.LoadLevel ("Game_Level");
+	}
+	
+	public void OnClickCancel()
+	{
+		Application.LoadLevel("Main");
+	}
+}
