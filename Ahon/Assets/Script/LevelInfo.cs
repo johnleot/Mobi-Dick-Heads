899e8c6588f2536ca6009457ceab899cec448337
Leo <jftangarorang@gmail.com>
2015-03-19 00:34:34 +0800

    Added Info Window in Level Info Scene

--- Ahon/Assets/Script/LevelInfo.cs
+++ Ahon/Assets/Script/LevelInfo.cs
@@ -3,6 +3,8 @@ using System.Collections;
 
 public class LevelInfo : MonoBehaviour {
 
+	public GameObject infoWindow;
+
 	public void OnClickPlay()
 	{
 		Application.LoadLevel ("Game_Level");
@@ -11,5 +13,16 @@ public class LevelInfo : MonoBehaviour {
 	public void OnClickCancel()
 	{
 		Application.LoadLevel("Main");
+	}
+
+	public void disableInfoWindow()
+	{
+		infoWindow.SetActive (false);
+	}
+
+	public void enableInfoWindow()
+	{
+		if (!infoWindow.activeInHierarchy)
+			infoWindow.SetActive (true);
 	}
 }
