0d0e9c87ab2a8d106112eedabf5424bbb8ec1e15
AlexusCipher <ballester.alexander@gmail.com>
2015-03-21 11:39:39 +0800

    AB [11/21/15]
    Main Button listener and Level select listener

---
+++ Ahon/Assets/Script/LevelSelectionBtnListener.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelSelectionBtnListener : MonoBehaviour {
+
+	public void onLevelSelected () {
+		Application.LoadLevel ("Level_Info");
+	}
+}
