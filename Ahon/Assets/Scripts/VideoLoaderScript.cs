58ece55d9903f5559843f26b94d696b583536845
Leo <jftangarorang@gmail.com>
2015-04-03 00:20:06 +0800

    Video added in cinematics scene (works only on android).

---
+++ Ahon/Assets/Scripts/VideoLoaderScript.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VideoLoaderScript : MonoBehaviour {
+	public GameObject videoHolder;
+	// Use this for initialization
+	void Start () {
+	/*#if UNITY_EDITOR
+		{
+			videoHolder.SetActive(true);
+			MovieTexture movieTexture_ = (MovieTexture)videoHolder.GetComponent<Renderer>().material.mainTexture;
+			movieTexture_.Play();
+			Invoke ("LoadMain", 20f);
+		}
+	#elif UNITY_ANDROID
+		{
+			Debug.Log("Android stuff here");
+			StartCoroutine(LoadVideo ("animation.mp4"));
+			LoadMain ();
+		}
+	#else
+		LoadMain();
+	#endif*/
+		Debug.Log("Android stuff here");
+		StartCoroutine(LoadVideo ("animation.mp4"));
+		LoadMain ();
+	}
+
+	IEnumerator LoadVideo(string path)
+	{
+		//WWW link = new WWW (path);
+		//yield return link;
+		Handheld.PlayFullScreenMovie (path, Color.black, FullScreenMovieControlMode.Hidden,
+		                             FullScreenMovieScalingMode.AspectFill);
+		yield return new WaitForEndOfFrame ();
+		yield return new WaitForEndOfFrame ();
+	}
+
+	public void LoadMain()
+	{
+		Application.LoadLevel ("Main");
+	}
+}
