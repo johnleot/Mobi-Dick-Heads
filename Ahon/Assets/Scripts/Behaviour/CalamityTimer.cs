900fd7bd54dda38d857cd43e95ddf07402c29bf2
AlexusCipher <ballester.alexander@gmail.com>
2015-04-14 21:45:26 +0800

    Added Result and Pause window with scripts.

--- Ahon/Assets/Scripts/Behaviour/CalamityTimer.cs
+++ Ahon/Assets/Scripts/Behaviour/CalamityTimer.cs
@@ -6,49 +6,58 @@ using Assets.Scripts.Classes;
 
 namespace Assets.Scripts.Behaviour
 {
-    class CalamityTimer : MonoBehaviour
-    {
-        public float calamityTimeRemaining; //eta of calamity is fetched from database
-        public float calamityTimeToComplete; // calamity duration is fetched from database
-        public Image calamity;
-        public Image duration;
-        public Text over;
-
-        public LevelManager levelManager;
-        public Calamity[] calamities;
-
-        void Start()
-        {
-            int level = PlayerPrefs.GetInt("LevelSelected");
-            levelManager = new CalamityManager();
-            calamities = levelManager.GetLevel(level).Calamities.ToArray();
-
-            Calamity firstCalamity = calamities[0];
-            calamityTimeRemaining = firstCalamity.TimeOfArrival;
-            calamityTimeToComplete = firstCalamity.Duration;
-
-            //instantiate calamity
-
-        }
-        void Update()
-        {
-            if (calamityTimeRemaining > 0.0f)
-            {
-                calamity.fillAmount = Mathf.MoveTowards(calamity.fillAmount, 1.0f, Time.deltaTime / calamityTimeRemaining);
-                calamityTimeRemaining -= Time.deltaTime;
-            }
-            else
-            {
-                if (calamityTimeToComplete > 0.0f)
-                {
-                    duration.fillAmount = Mathf.MoveTowards(duration.fillAmount, 1.0f, Time.deltaTime / calamityTimeToComplete);
-                    calamityTimeToComplete -= Time.deltaTime;
-                }
-                else
-                {
-                    over.text = "Game Over";
-                }
-            }
-        }
-    }
+	class CalamityTimer : MonoBehaviour
+	{
+		public float calamityTimeRemaining; //eta of calamity is fetched from database
+		public float calamityTimeToComplete; // calamity duration is fetched from database
+		public Image calamity;
+		public Image duration;
+		public Text over;
+		
+		public LevelManager levelManager;
+		public Calamity[] calamities;
+		
+		private GameObject ResultsWindow;
+		
+		void Awake(){
+			ResultsWindow = GameObject.FindGameObjectWithTag ("ResultsWindow");
+		}
+		void Start()
+		{
+			int level = PlayerPrefs.GetInt("LevelSelected");
+			levelManager = new CalamityManager();
+			calamities = levelManager.GetLevel(level).Calamities.ToArray();
+			
+			Calamity firstCalamity = calamities[0];
+			calamityTimeRemaining = firstCalamity.TimeOfArrival;
+			calamityTimeToComplete = firstCalamity.Duration;
+			
+			ResultsWindow.SetActive (false);
+			//instantiate calamity
+			
+		}
+		void Update()
+		{
+			if (calamityTimeRemaining > 0.0f)
+			{
+				calamity.fillAmount = Mathf.MoveTowards(calamity.fillAmount, 1.0f, Time.deltaTime / calamityTimeRemaining);
+				calamityTimeRemaining -= Time.deltaTime;
+			}
+			else
+			{
+				if (calamityTimeToComplete > 0.0f)
+				{
+					duration.fillAmount = Mathf.MoveTowards(duration.fillAmount, 1.0f, Time.deltaTime / calamityTimeToComplete);
+					calamityTimeToComplete -= Time.deltaTime;
+				}
+				else
+				{
+					over.text = "Game Over";
+					if(!ResultsWindow.activeInHierarchy){
+						ResultsWindow.SetActive(true);
+					}
+				}
+			}
+		}
+	}
 }
