eec46380af7238d1e611a6311deea29e9c63cd4f
Leo <jftangarorang@gmail.com>
2015-06-05 11:36:39 +0800

    Added AstarPathfinding

---
+++ Ahon/Assets/Scripts/AstarAI.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+using Pathfinding;
+
+public class AstarAI : MonoBehaviour {
+
+	public Vector3 targetPosition;
+
+	private Seeker seeker;
+	private CharacterController controller;
+
+	public Path path;
+
+	public float speed = 100;
+
+	public float nextWaypointDistance = 3;
+
+	private int currentWaypoint = 0;
+
+
+
+	// Use this for initialization
+	void Start () {
+		//Get a reference to the Seeker component we added earlier
+		seeker = GetComponent<Seeker>();
+		controller = GetComponent<CharacterController> ();
+		//Start a new path to the targetPosition, return the result to the OnPathComplete function
+		seeker.StartPath (transform.position,targetPosition, OnPathComplete);
+	}
+	
+	public void OnPathComplete (Path p) {
+		Debug.Log ("Yay, we got a path back. Did it have an error? " + p.error);
+		if (!p.error) {
+			path = p;
+			//Reset the waypoint counter
+			currentWaypoint = 0;
+		}
+	}
+
+	public void FixedUpdate () {
+		if (path == null) {
+			//We have no path to move after yet
+			return;
+		}
+		
+		if (currentWaypoint >= path.vectorPath.Count) {
+			Debug.Log ("End Of Path Reached");
+			return;
+		}
+		
+		//Direction to the next waypoint
+		Vector3 dir = (path.vectorPath[currentWaypoint]-transform.position).normalized;
+		dir *= speed * Time.fixedDeltaTime;
+		controller.SimpleMove (dir);
+		
+		//Check if we are close enough to the next waypoint
+		//If we are, proceed to follow the next waypoint
+		if (Vector3.Distance (transform.position,path.vectorPath[currentWaypoint]) < nextWaypointDistance) {
+			currentWaypoint++;
+			return;
+		}
+	}
+}
