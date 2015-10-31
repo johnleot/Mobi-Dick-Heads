cbd0856e2734904a08e1ee7e670bc1c41f6a708e
Leo <jftangarorang@gmail.com>
2015-11-01 00:55:39 +0800

    getter and setter for the MAX number of occupants per dwelling.

--- Ahon/Assets/Scripts/Classes/IObject.cs
+++ Ahon/Assets/Scripts/Classes/IObject.cs
@@ -8,10 +8,20 @@ public abstract class IObject: MonoBehaviour {
 	
 	protected string title_;
 	protected string contentText_;
-	protected int someInt;
+	protected int maxNumberofOccupants_;
+	//protected int someInt;
 
 	public abstract void insertUI();
 	public abstract void showUI();
 	public abstract void hideUI();
 	public abstract void removeUI();
+
+	public int MaxNumberofOccupants_ {
+		get {
+			return maxNumberofOccupants_;
+		}
+		set {
+			maxNumberofOccupants_ = value;
+		}
+	}
 }
